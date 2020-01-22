6eb41cfb26e4ea324f873e9bfad2eeb317974804
thinh <duc-thinh.ngo@polytechnique.edu>
2020-01-22 19:29:44 +0100

    Added method ExactCover.solveByDancingLinks

--- src/polyomino/DancingLinks.java
+++ src/polyomino/DancingLinks.java
@@ -5,17 +5,21 @@ import java.util.*;
 //One trouble is to initialize a DataObject which points to itself. We cannot do it in a construct function
 //so I initialize an DataObject pointing to null first then, after initializing, I modify the links so that
 //it points to itself.
-public class DancingLinks {
-	HashMap<Integer, ColumnObject> columns;
-	ColumnObject head;
+public class DancingLinks{
+	private HashMap<Integer, ColumnObject> columns;
+	private HashMap<Integer, Object> hash;
+	public ColumnObject head;
 	
 	
 	<E> DancingLinks(Set<E> X, Set<Set<E>> C){
 		columns = new HashMap<>();
 		head = new ColumnObject();
+		hash = new HashMap<>();
 		head.U = head.D = head.L = head.R = head;
+		head.S = Integer.MAX_VALUE;
 		for(E x: X) {
 			ColumnObject column = new ColumnObject(x);
+			hash.put(x.hashCode(),x);
 			column.U = column.D = column.L = column.R = column;
 			add(column);
 		}
@@ -24,6 +28,39 @@ public class DancingLinks {
 			add(subset);
 		}
 	}
+	
+	public void coverColumn(ColumnObject x) {
+		x.R.L = x.L;
+		x.L.R = x.R;
+		DataObject iteratorColumn = x.D;
+		while(iteratorColumn != x ) {
+			DataObject iteratorRow = iteratorColumn.R;
+			while(iteratorRow != iteratorColumn) {
+				iteratorRow.U.D = iteratorRow.D;
+				iteratorRow.D.U = iteratorRow.U;
+				iteratorRow.C.S --;
+				iteratorRow = iteratorRow.R;
+			}
+			iteratorColumn = iteratorColumn.D;
+		}
+	}
+	
+	public void uncoverColumn(ColumnObject x) {
+		x.R.L = x;
+		x.L.R = x;
+		DataObject iteratorColumn = x.U;
+		while(iteratorColumn != x) {
+			DataObject iteratorRow = iteratorColumn.R;
+			while(iteratorRow != iteratorColumn) {
+				iteratorRow.U.D = iteratorRow;
+				iteratorRow.D.U = iteratorRow;
+				iteratorRow.C.S ++;
+				iteratorRow = iteratorRow.R;
+			}
+			iteratorColumn = iteratorColumn.U;
+		}
+	}
+	
 	
 	public void add(ColumnObject column){
 		this.columns.put(column.N, column);
@@ -52,6 +89,7 @@ public class DancingLinks {
 		node.D = column;
 		column.U.D = node;
 		column.U = node;
+		node.C = column;
 		column.S ++;
 	}
 	
@@ -68,6 +106,11 @@ public class DancingLinks {
 		node.L.R = node.R;
 		node.R.L = node.L;
 		node.U = node.D = node.L = node.R = null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <E> E getObject(int hashCode) {
+		return (E)this.hash.get(hashCode);
 	}
 	
 }
--- src/polyomino/ExactCover.java
+++ src/polyomino/ExactCover.java
@@ -33,6 +33,71 @@ public class ExactCover {
 		return result;
 	}
 	
+	public static <E> Set<Set<Set<E>>> solveByDancingLinks(Set<E> X, Set<Set<E>> C){
+		DancingLinks dancingLinks = new DancingLinks(X, C);
+		Set<Set<DataObject>> P = solveByDancingLinks(dancingLinks);
+		Set<Set<Set<E>>> result = new HashSet<>();
+		for(Set<DataObject> p: P) {
+			Set<Set<E>> solution = new HashSet<>();
+			for(DataObject head: p) {
+				Set<E> subset = new HashSet<>();
+				DataObject iteratorRow = head;
+				do {
+					subset.add(dancingLinks.getObject(iteratorRow.C.N));
+					iteratorRow = iteratorRow.R;
+				}while(iteratorRow != head);
+				solution.add(subset);
+			}
+			result.add(solution);
+		}
+		return result;
+			
+	}
+	
+	public static Set<Set<DataObject>> solveByDancingLinks(DancingLinks H){
+		Set<Set<DataObject>> result = new HashSet<>();
+		if(H.head.R == H.head) {
+			result.add(new HashSet<DataObject>());
+			return result;
+		}
+		
+		ColumnObject x = (ColumnObject) H.head.R;
+		ColumnObject iterator = (ColumnObject) H.head.R;
+		int minS = iterator.S;
+		while(iterator != H.head) {
+			if(minS > iterator.S) {
+				x = iterator;
+				minS = x.S;
+			}
+			iterator = (ColumnObject) iterator.R;
+		}
+		//Finish choosing x with minimal size
+		H.coverColumn(x);
+		DataObject t = x.U;
+		while(t != x) {
+			DataObject y = t.L;
+			while(y != t) {
+				H.coverColumn(y.C);
+				y = y.L;
+			}
+			
+			for(Set<DataObject> P: solveByDancingLinks(H)) {
+				P.add(t);
+				result.add(P);
+			}
+			
+			y = t.R;
+			while(y != t) {
+				H.uncoverColumn(y.C);
+				y = y.R;
+			}
+			
+			t = t.U;
+		}
+		H.uncoverColumn(x);
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		Set<Integer> X = new HashSet<>();
 		for(int i=1; i<=7; i++)
@@ -59,11 +124,24 @@ public class ExactCover {
 		}
 		*/
 		
-		DancingLinks dl = new DancingLinks(X,C);
+/*		DancingLinks dl = new DancingLinks(X,C);
+		ColumnObject x = (ColumnObject) dl.head.R.R;
+		dl.coverColumn(x);
+		dl.uncoverColumn(x);
 		ColumnObject column = (ColumnObject) dl.head.R;
 		while(column != dl.head) {
 			System.out.println(column.S);
 			column = (ColumnObject) column.R;
+		}
+*/
+		Set<Set<Set<Integer>>> exactCover = solveByDancingLinks(X,C);
+		for(Set<Set<Integer>> P: exactCover) {
+			for(Set<Integer> S: P) {
+				for(Integer y: S)
+					System.out.print(y + " ");
+				System.out.print("\n");
+			}
+			System.out.println("\n");
 		}
 	}
 }
